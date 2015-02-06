c81fd255075082351e9041f07a3cfa0c08360c5a
Jonathan Williamson <jonathan@cgcookie.com>
2015-02-06 13:03:55 -0600

    Finish refactor. Should be working now.

--- cgc-segment.php
+++ cgc-segment.php
@@ -6,8 +6,6 @@
  * Author URI: http://cgcookie.com
  * Version: 1.0.0
  */
-# Setup our Segment tracking and
-# alias to Analytics for convenience
 
 class cgcSegment {
 
@@ -22,37 +20,48 @@ class cgcSegment {
 			require_once dirname( __FILE__ ) . "/includes/rcp.php";
 		}
 
+		# Setup our Segment tracking and
+		# alias to Analytics for convenience
 		class_alias('Segment', 'Analytics');
 		Analytics::init("jOMIQl4Nqe4zzkUNITBHlyKKVixnTpTl");
 
 	}
 
-	public static function identify_user() {
-		$user_id = get_current_user_id();
-		$user = get_userdata( $user_id );
+	public static function identify_user( $traits = array() ) {
 
-		Analytics::identify(array(
-			"userId" => $user_id,
-			"traits" => array(
+		# Check for traits
+		if( empty( $traits ) ) {
+			$user_id = get_current_user_id();
+			$user = get_userdata( $user_id );
+
+			$traits = array(
 				"firstName" => $user->first_name,
 				"lastName" => $user->last_name,
 				"email" => $user->user_email,
-				"username" => $user->user_login,
-				)
-			)
-		);
-		return $user_id;
-	}
+				"username" => $user->user_login
+				);
+		}
 
+		# User data to be passed
+		$args = array(
+			"userId" => $user_id,
+			"traits" => $traits
+		);
+		Analytics::identify( $args );
+		return $args;
+	}
 
 	public static function track( $event = '', $properties = array() ) {
 
+		# If no event name or properties are passed, return
 		if( empty( $event ) || empty( $properties ) ) {
 			return false;
 		}
 
+		$userdata = self::identify_user();
+
 		Analytics::track(array(
-				"userId" => self::identify_user(),
+				"userId" => $userdata['userId'],
 				"event" => $event,
 				"properties" => $properties
 			)
--- includes/edd.php
+++ includes/edd.php
@@ -28,20 +28,6 @@ function cgc_edd_track_add_product_to_cart( $download_id, $options ) {
 	);
 
 	cgcSegment::track( 'Added Product', $properties );
-
-	// $user_id = self::identify_user();
-
-	// Analytics::track(array(
-	// 	"userId" => $user_id,
-	// 	"event" => "Added Product",
-	// 	"properties" => array(
-	// 		"id" => $download_id,
-	// 		"name" => get_the_title( $download_id ),
-	// 		"price" => edd_get_cart_item_price( $download_id, $options ),
-	// 		"category" => self::cgc_get_product_category( $download_id )
-	// 		)
-	// 	)
-	// );
 }
 add_action( 'edd_post_add_to_cart', 'cgc_edd_track_add_product_to_cart', 1, 2 );
 
@@ -61,16 +47,6 @@ function cgc_edd_track_remove_product_from_cart( $cart_key ) {
 
 	cgcSegment::track( 'Removed Product', $properties );
 
-	// Analytics::track(array(
-	// 	"userId" => $user_id,
-	// 	"event" => "Removed Product",
-	// 	"properties" => array(
-	// 		"id" => $download_id,
-	// 		"name" => get_the_title( $download_id ),
-	// 		"price" => edd_get_cart_item_price( $download_id, $options ),
-	// 		)
-	// 	)
-	// );
 }
 add_action( 'edd_pre_remove_from_cart', 'cgc_edd_track_remove_product_from_cart' );
 
@@ -82,6 +58,13 @@ add_action( 'edd_pre_remove_from_cart', 'cgc_edd_track_remove_product_from_cart'
 */
 function cgc_edd_track_purchase( $payment_id ) {
 	$user_id = get_current_user_id();
+
+	$userInfo = edd_get_payment_meta_user_info( $payment_id);
+
+	$traits = array(
+		"firstName" => $userInfo[ 'first_name' ],
+		"lastName" => $userInfo[ 'last_name' ]
+		);
 
 	if( ! class_exists( 'Easy_Digital_Downloads' ) ) {
 		return false;
@@ -118,24 +101,6 @@ function cgc_edd_track_purchase( $payment_id ) {
 		);
 
 	cgcSegment::track( 'Completed Order', $properties );
-
-
-	// Analytics::track(array(
-	// 	"userId" => $user_id,
-	// 	"event" => "Completed Order",
-	// 	"properties" => array(
-	// 		"orderId" => $payment_id,
-	// 		"total" => $subtotal,
-	// 		"revenue" => $total,
-	// 		"currency" => "USD",
-	// 		"tax" => $tax,
-	// 		"discount" => $subtotal - $total, // total - coupon amount
-	// 		"coupon" => $discounts,
-	// 		"repeat" => $repeat,
-	// 		"products" => $products
-	// 		)
-	// 	)
-	// );
 }
 add_action( 'edd_complete_purchase', 'cgc_edd_track_purchase', 9999, 1 );
 
