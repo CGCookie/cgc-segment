9707f0a66ed96ba6f70d19abf80205d34f1d771e
Jonathan Williamson <jonathan@cgcookie.com>
2015-02-10 14:43:44 -0600

    Comment out all RCP tracking for now.

--- includes/rcp.php
+++ includes/rcp.php
@@ -1,49 +1,56 @@
 <?php
 
-# Stripe upgrades. This should become universal and pay payment details instead.
-function cgc_rcp_account_upgrade( $payment_id) {
+// # Stripe upgrades. This should become universal and pay payment details instead.
+// function cgc_rcp_account_upgrade( $payment_id) {
 
-	$user_id = get_current_user_id();
+// 	$user_id = get_current_user_id();
 
-	$subscription = rcp_get_subscription( $user_id );
-	$expiration = rcp_get_expiration_date( $user_id );
-	$recurring = rcp_is_recurring( $user_id ) ? 'Yes' : 'No';
-	$rcp_payments = new RCP_Payments;
-	$new_user = $rcp_payments->last_payment_of_user( $user_id );
-	$user_time = strtotime( $user->user_registered, current_time( 'timestamp' ) );
-	$renewal = ! empty( $new_user );
-	$upgrade = $user_time < $ten_min_ago && ! $renewal ? true : false;
-	$discount = '';
+// 	$subscription = rcp_get_subscription( $user_id );
+// 	$expiration = rcp_get_expiration_date( $user_id );
+// 	$recurring = rcp_is_recurring( $user_id ) ? 'Yes' : 'No';
+// 	$rcp_payments = new RCP_Payments;
+// 	$new_user = $rcp_payments->last_payment_of_user( $user_id );
+// 	$user_time = strtotime( $user->user_registered, current_time( 'timestamp' ) );
+// 	$renewal = ! empty( $new_user );
+// 	$upgrade = $user_time < $ten_min_ago && ! $renewal ? true : false;
+// 	$discount = '';
 
-	$traits = array(
-		'firstName' = '',
-		'lastName' = '',
-		'email' = '',
-		'username' = '',
-		'account type' = 'Citizen',
-		'account status' = 'Active',
-		'account level' = $subscription,
-		'recurring' = $recurring,
-		'expiration' = $expiration
-		);
+// 	// $traits = array(
+// 	// 	'firstName' = '',
+// 	// 	'lastName' = '',
+// 	// 	'email' = '',
+// 	// 	'username' = '',
+// 	// 	'account type' = 'Citizen',
+// 	// 	'account status' = 'Active',
+// 	// 	'account level' = $subscription,
+// 	// 	'recurring' = $recurring,
+// 	// 	'expiration' = $expiration
+// 	// 	);
 
-	if( ! empty( $_REQUEST['rcp_discount'])){ 
-		$discount = sanitize_text_field( $_REQUEST['rcp_discount']);
-		}
+// 	if( ! empty( $_REQUEST['rcp_discount'] ) ) { 
+// 		$discount = sanitize_text_field( $_REQUEST['rcp_discount'] );
+// 	}
 
-	$properties = array(
-		'account type' = 'Citizen';
-		'account status' = 'Active';
-		'account level' = $subscription;
-		'redeemed gift' = 'No';
-		'coupon' = $discount;
-		'recurring' = $recurring;
-		'expiration' = $expiration;
-		'renewal' = $renewal ? 'Yes' : 'No';
-		'Time Since Creation'] = human_time_diff( $user_time, current_time( 'timestamp' ) );
-		);
+// 	// $properties = array(
+// 	// 	'account type' = 'Citizen',
+// 	// 	'account status' = 'Active',
+// 	// 	'account level' = $subscription,
+// 	// 	'redeemed gift' = 'No',
+// 	// 	'coupon' = $discount,
+// 	// 	'recurring' = $recurring,
+// 	// 	'expiration' = $expiration,
+// 	// 	'renewal' = $renewal ? 'Yes' : 'No',
+// 	// 	'Time Since Creation' = human_time_diff( $user_time, current_time( 'timestamp' ) ),
+// 	// 	);
 
-	cgcSegment::track( 'Account Upgraded', $properties );
+// 	cgcSegment::track( 'Account Upgraded', $properties );
 
-}
-add_action( 'rcp_stripe_signup', 'cgc_rcp_account_upgrade', 10, 2 );
+// }
+// add_action( 'rcp_stripe_signup', 'cgc_rcp_account_upgrade', 10, 2 );
+
+
+# function cgc_rcp_account_cancelled
+
+# function cgc_rcp_payment
+
+# function cgc_rcp_account_status
