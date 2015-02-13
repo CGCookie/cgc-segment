93250426b044b7eeb9eb2dadf00372acb62e0d3c
Jonathan Williamson <jonathan@cgcookie.com>
2015-02-12 19:45:29 -0600

    Add Citizen status checks to login event.

--- includes/user_login.php
+++ includes/user_login.php
@@ -2,11 +2,24 @@
 
 function cgc_track_login( $user_login, $user ) {
 
+	$user_id = $user->ID;
+
 	$traits = array(
-		"userId" => $user->ID
+		'userId' => $user->ID,
 		);
+
+	if( function_exists( 'rcp_get_subscription' ) ) {
+		$subscription = rcp_get_subscription( $user_id );
+
+		$traits['account type'] = rcp_is_active( $user_id ) ? 'Citizen' : 'Basic';
+		$traits['account status'] = ucwords( rcp_get_status( $user_id ) );
+		$traits['account level'] = $subscription;
+	}
+
+	$properties = array();
+
 	cgcSegment::identify_user( $traits );
-	cgcSegment::track( 'User Login' );
+	cgcSegment::track( 'User Login', $properties, $traits );
 
 }
 add_action('wp_login', 'cgc_track_login', 9999, 2);
