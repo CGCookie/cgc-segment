84dd3d0e4c34b1aab6cd879fd2b941d59870c1cb
Jonathan Williamson <jonathan@cgcookie.com>
2015-03-02 13:19:12 -0600

    Add properties to login event for mixpanel segmentation.

--- includes/user_login.php
+++ includes/user_login.php
@@ -13,15 +13,25 @@ function cgc_track_login( $user_login, $user ) {
 		'email' => $user_data->user_email
 		);
 
+	$properties = array(
+		'userId' => $user_id,
+		'firstName' => $user_data->first_name,
+		'lastName' => $user_data->last_name,
+		'username' => $user_login,
+		'email' => $user_data->user_email
+		);
+
 	if( function_exists( 'rcp_get_subscription' ) ) {
 		$subscription = rcp_get_subscription( $user_id );
 
 		$traits['type'] = rcp_is_active( $user_id ) ? 'Citizen' : 'Basic';
 		$traits['status'] = ucwords( rcp_get_status( $user_id ) );
 		$traits['level'] = $subscription;
-	}
 
-	$properties = array();
+		$properties['type'] = rcp_is_active( $user_id ) ? 'Citizen' : 'Basic';
+		$properties['status'] = ucwords( rcp_get_status( $user_id ) );
+		$properties['level'] = $subscription;
+	}
 
 	cgcSegment::identify_user( $user_id, $traits );
 	cgcSegment::track( 'User Login', $properties, $traits, $user_id);
