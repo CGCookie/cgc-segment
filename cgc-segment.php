cb548438692c5e9115829610183355211b6d76f4
Jonathan Williamson <jonathan@cgcookie.com>
2015-05-22 11:53:53 -0500

    Refactor account creation tracking. Improves #22.

--- cgc-segment.php
+++ cgc-segment.php
@@ -32,6 +32,7 @@ class cgcSegment {
 		}
 		require_once dirname( __FILE__ ) . "/includes/pageviews.php";
 		require_once dirname( __FILE__ ) . "/includes/user_login.php";
+		require_once dirname( __FILE__ ) . "/includes/user_registration.php";
 		// require_once dirname( __FILE__ ) . "/includes/segment_js.php";
 
 		# Setup our Segment tracking and
