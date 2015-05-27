974dea72afea0ce5d6b1a5414340c4fc0ecd0651
Jonathan Williamson <jonathan@cgcookie.com>
2015-05-27 16:31:56 -0500

    Track Questions asked. Close #27.

--- includes/user_actions.php
+++ includes/user_actions.php
@@ -40,7 +40,26 @@ function cgc_track_flows_dropped( $user_id, $post_id ) {
 }
 add_action( 'flow_dropped', 'cgc_track_flows_dropped', 10, 2 );
 
+
 # Track Questions
+function cgc_track_question_asked( $user_id, $comment_id ) {
+
+	$question = get_the_title( $comment_id );
+
+	$properties = array(
+		'userId' => $user_id,
+		'question'   => $question,
+		);
+
+	$traits = array(
+		'userId'    => $user_id,
+		'question'   => $question,
+		);
+
+	cgcSegment::track( 'Asked Question', $properties, $traits, $user_id );
+
+}
+add_action( 'question_added', 'cgc_track_question_asked', 10, 2 );
 
 
 # Track Bookmarks
