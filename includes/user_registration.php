9be135a1fdeab3e33f161064410b3426300d9bfc
Jonathan Williamson <jonathan@cgcookie.com>
2015-08-27 16:55:07 -0500

    Add function check for RCP status in user registration

--- includes/user_registration.php
+++ includes/user_registration.php
@@ -7,22 +7,26 @@ function cgc_track_user_registration( $user_id ) {
 	$user_data  = get_userdata( $user_id );
 	$registered = ($user_data->user_registered . "\n");
 
-	$status = ucwords( rcp_get_status( $user_id ) );
+	if( function_exists( 'rcp_is_active' ) ) {
+		$status = ucwords( rcp_get_status( $user_id ) );
+	}
 
 	$traits = array(
 		'firstName' => $user_data->first_name,
 		'lastName'  => $user_data->last_name,
 		'email'     => $user_data->user_email,
 		'username'  => $user_data->user_login,
-		'status'    => $status,
 		'createdAt' => date("n/j/Y", strtotime($registered))
 		);
 
 	$properties = array(
-		'status'      => $status,
 		'createdAt'   => date("n/j/Y", strtotime($registered))
 		);
 
+	if( function_exists( 'rcp_is_active' ) ) {
+		$traits['status'] = $status;
+		$properties['status'] = $status;
+	}
 
 	cgcSegment::identify_user( $user_id, $traits );
 	cgcSegment::track( 'Account Created', $properties, $traits, $user_id );
