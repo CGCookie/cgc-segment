d2d3a22f4a8e52778933b53906f52aea090880f6
Jonathan Williamson <jonathan@cgcookie.com>
2015-09-02 10:59:42 -0500

    Remove EDD pageview code, should go in EDD instead. Close #21

--- includes/pageviews.php
+++ includes/pageviews.php
@@ -2,10 +2,6 @@
 
 function cgc_track_pageviews() {
 
-	// global $edd_options;
-	// $checkout = $edd_options['purchase_page'];
-	// $purchase_history_page =$edd_options['purchase_history_page'];
-
 	$properties = array();
 
 	if( is_user_logged_in() ) {
