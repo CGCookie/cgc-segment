c9103de231733dd7cbc227264e46004b291d62d6
Jonathan Williamson <jonathan@cgcookie.com>
2015-10-13 11:47:27 -0500

    Convert dates to UTC string for proper date formatting in Intercom.

--- includes/pageviews.php
+++ includes/pageviews.php
@@ -7,16 +7,20 @@ function cgc_track_pageviews() {
 	if( is_user_logged_in() ) {
 	?>
 		<script type="text/javascript">
+
+			var expiration = new Date( cgc_analytics_vars.expiration );
+			var createdAt = new Date( cgc_analytics_vars.createdAt );
+
 			analytics.identify( cgc_analytics_vars.userId, {
 				firstName: cgc_analytics_vars.firstName,
 				lastName: cgc_analytics_vars.lastName,
 				email: cgc_analytics_vars.email,
-				createdAt: cgc_analytics_vars.createdAt,
+				createdAt: createdAt.toUTCString(),
 				userRoles: cgc_analytics_vars.userRoles,
 				type: cgc_analytics_vars.type,
 				status: cgc_analytics_vars.status,
 				level: cgc_analytics_vars.level,
-				expiration: cgc_analytics_vars.expiration,
+				expiration: expiration.toUTCString(),
 				subjects: cgc_analytics_vars.subjects,
 				topics: cgc_analytics_vars.topics,
 			}
