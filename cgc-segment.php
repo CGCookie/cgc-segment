9d7d49ac550ccfbc2036e0e130894deb9cdec0ca
Jonathan Williamson <jonathan@cgcookie.com>
2015-11-12 14:08:51 -0600

    Version bump 1.4.4

--- cgc-segment.php
+++ cgc-segment.php
@@ -4,7 +4,7 @@
  * Description: Sends CGC data to segment.io
  * Author: Jonathan Williamson
  * Author URI: http://cgcookie.com
- * Version: 1.4.3
+ * Version: 1.4.4
  */
 
 $plugin_url = WP_PLUGIN_URL . '/cgc-segment';
